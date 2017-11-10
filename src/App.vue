e99df29f4bac6489c9f57a674bf06e10e453bae7
Leon Wang <wangliang1124@163.com>
2017-11-10 18:03:52 +0800

    创建eleme项目

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <header>我是header</header>
+    <div class="content-wrapper">
+      我是内容
+    </div>
+    <footer>
+      我是页脚
+    </footer>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header/Header'
+
+  export default {
+    name: 'app',
+    components: {
+      Header
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
