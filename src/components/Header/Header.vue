e8443c8bfb5f6f55531eac8d2405a0a9bbea60b2
Leon Wang <wangliang1124@163.com>
2017-11-14 13:52:25 +0800

    add code

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -9,11 +9,15 @@
           <span class="brand"></span>
           <span class="name">{{seller.name}}</span>
         </div>
-        <div class="description">
-          {{seller.description}}/{{seller.deliveryTime}} 分钟送达
-        </div>  <!-- delivery -->
-        <div class="support"></div>      <!-- discounts -->
-        <div class="support-count"></div> <!-- discounts-count -->
+        <div class="description"> <!-- delivery -->
+          {{seller.description}}/{{seller.deliveryTime}} 分钟送达   
+        </div> 
+        <div class="support">
+          <span class="type"></span>
+          <span class="desc">{{seller.supports[0].description}}</span>
+        </div>      <!-- discounts -->
+        <div class="support-count">{{seller.supports.length}}个</div> <!-- discounts-count -->
+        <i class="icon-keyboard_arrow_right"></i>
       </div> 
     </div>
   </div>
@@ -30,7 +34,9 @@
   }
 </script>
 <style type="text/css" lang="stylus" scoped>
+  // @import('../../assets/stylus/iconfont.styl')
   .header
+    position: relative
     .content-wrapper
       padding: 24px 12px 18px 24px
       font-size: 0
@@ -41,16 +47,39 @@
         background-color: #ccc
       .content
         display: inline-block
+        vertical-align: top
         .title
           .brand
             display: inline-block
             width: 30px
             height: 18px
-            vertical-align:top
+            margin-right: 6px
+            vertical-align: bottom
             background:url(brand@3x.png) no-repeat
             background-size: 30px 18px
           .name
             font-size: 16px
         .description
-          font-size: 16px
+          font-size: 12px
+          margin: 8px 0 10px 0
+        .support
+          .type
+            display: inline-block
+            width: 16px
+            height: 16px
+            margin-right: 4px
+            vertical-align: bottom
+            background: url(decrease_1@3x.png) no-repeat 
+            background-size: 16px 16px
+          .desc
+            font-size: 10px
+    .support-count
+      position: absolute
+      display: block
+      right: 12px
+      bottom: 18px
+      padding: 7px 8px
+      border-radius: 8px
+      font-size: 10px
+      background-color: rgba(0,0,0,0.2)
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-// import Axios from 'axios'
 import App from '@/App'
+// require('./assets/stylus/base.styl')
+
 
 Vue.config.productionTip = false
 
-// Vue.use(Axios)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
