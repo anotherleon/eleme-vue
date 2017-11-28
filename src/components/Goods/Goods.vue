c1d7bb84723c65651be5a995b1e4d9124508f92f
wangliang1124 <wangliang1124@163.com>
2017-11-28 21:16:38 +0800

    000

--- src/components/Goods/Goods.vue
+++ src/components/Goods/Goods.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="goods">
-    我是商品
+  <div class="goods" @click="increase">
+    我是商品 {{count}}
   </div>
 </template>
 <script type="text/javascript">
   export default {
     name: 'Goods',
+    data() {
+      return {
+        count: 0,
+      }
+    },
+    methods:{
+      increase() {
+        this.count ++
+      },
+    },
   }
 </script>
 <style type="text/css" lang="stylus" scoped>
